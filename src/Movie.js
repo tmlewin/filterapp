bf3f253b6fdb12cfb38c8e1fe56d3e80bf1f02c8
tmlewin <dexterity4u@gmail.com>
2022-03-10 12:55:10 -0600

    "init"

---
+++ src/Movie.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {motion} from 'framer-motion'
+
+const Movie = ({movie}) => {
+  return (
+    <motion.div 
+    animate={{opacity : 1}} 
+    initial={{opacity: 0}} 
+    exit={{opacity: 0}}
+    layout>
+        <h2>{movie.title}</h2>
+        <img 
+        src={'https://image.tmdb.org/t/p/w500' + movie.backdrop_path} 
+        alt=""/>
+    </motion.div>
+  )
+}
+
+export default Movie
